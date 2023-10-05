bcfdd2c21883ab1df905327e218fa86b420643f8
Sing Tuttanon <singtuttanon@gmail.com>
2023-10-05 14:26:53 -0400

    Updated Payroll Calculator with overtime pay

--- src/main/java/com/pluralsight/PayRollCalculator.java
+++ src/main/java/com/pluralsight/PayRollCalculator.java
@@ -1,5 +1,29 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayRollCalculator {
-    // Will work on this after I finish previous assignments.
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user for their name
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        // Prompt the user for hours worked
+        System.out.print("Enter hours worked: ");
+        double hoursWorked = input.nextDouble();
+
+        // Prompt the user for pay rate
+        System.out.print("Enter pay rate per hour: ");
+        double payRate = input.nextDouble();
+
+        // Calculate gross pay
+        double grossPay = hoursWorked * payRate;
+
+        // Display the employee's name and gross pay
+        System.out.println("Employee: " + name);
+        System.out.println("Gross Pay: $" + grossPay);
+    }
+
 }
